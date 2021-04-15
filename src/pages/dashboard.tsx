4aefc5d3b96026939d33b7f471e75056d66723f0
Pedro Lages <pedro.lages@hotmart.com>
2021-04-14 22:20:23 -0300

    creates Header and Dashboard components

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,12 @@
+import { Flex } from '@chakra-ui/react'
+import { Header } from '../components/Header'
+
+interface DashboardProps {
+
+}
+
+export default function Dashboard({}: DashboardProps) {
+    return (
+        <Header />
+    )
+}
